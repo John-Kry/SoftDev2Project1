1e3eacbd5b2d6041ec8c0fa662823082f9fda25e
MORourke3 <mjo95jr@aol.com>
2015-03-04 20:54:49 -0500

    Softdev 2 Project 1 w/ comments
    Added comments and finalized code

--- index.php
+++ index.php
@@ -1,10 +1,19 @@
 <?php
+// John and Mike worked together on the project
+// Collaborated directly on all parts of the document
+// Both gave equal input and worked through the entire project over Skype
+
+
+// Starts a new session in which to store variables
 session_start();
 if (isset($_POST["Submit"])){
+	// allows the input of name, CWID 
      $_SESSION['fullName'] = $_POST['fullName'];
      $_SESSION['CWID'] = $_POST['CWID'];
      $_SESSION['Residency'] = $_POST['Residency'];
+			// if no selection for housing was chosen iterate down to the next form to move on
            if ($_POST["Residency"]=="noSelection"){
+				// freshman can not choose a townhouse or kitchen so offer them these options
                if (($_POST["Year"]=="Frosh")&&((isset($_POST["Townhouse"]))||(isset($_POST["Kitchen"])))){
                echo ("There is no configuration that exists for these choices.<br>");
                    echo (' Here are some options for freshman:<br> 
@@ -17,6 +26,7 @@ if (isset($_POST["Submit"])){
                    </select><br><br>
                    <INPUT TYPE = "Submit" Name = "Submit2" VALUE = "Submit">
                    </Form>');
+				// if they are a freshman offer then these options
                }else if ($_POST["Year"]=="Frosh"){
                    echo (' Here are some options for freshman:<br> 
                    <Form Name ="toConfirmPage" Method ="POST" ACTION = "index.php">
@@ -28,31 +38,34 @@ if (isset($_POST["Submit"])){
                    </select><br><br>
                    <INPUT TYPE = "Submit" Name = "Submit2" VALUE = "Submit">
                    </Form>');
+				// if they are a sophomore and don't want a kitchen or town here are their options
                }else if ($_POST["Year"]=="Soph"&& ((!isset($_POST["Kitchen"]))&&(!isset($_POST["Townhouse"])))){
                    echo ('Here are some options for sophomores:<br> 
-                   <Form Name ="toConfirmPage" Method ="POST" ACTION = "index.php">
-                   <select name="Residency">
-                   <option value="Midrise">Midrise Hall</option>
+                    <Form Name ="toConfirmPage" Method ="POST" ACTION = "index.php">
+                    <select name="Residency">
+                    <option value="Midrise">Midrise Hall</option>
                     <option value="Foy">Foy Townhouses</option>
                     <option value="Gartland">Gartland Commons</option>
                     <option value="NewTownhouses">New TownHouses</option>
                     </select><br><br>
                     <INPUT TYPE = "Submit" Name = "Submit2" VALUE = "Submit">
                     </Form>');
+				// if they are a sophomore and want a kitchen or townhouse these are the options
                }else if (($_POST["Year"]=="Soph") &&((isset($_POST["Kitchen"]))||(isset($_POST["Townhouse"])))){
                    echo ('Here are some options for sophomores, with a kitchen and or a townhouse:<br> 
                     <Form Name ="toConfirmPage" Method ="POST" ACTION = "index.php">
                     <select name="Residency">
-                   <option value="Foy">Foy Townhouses</option>
+                    <option value="Foy">Foy Townhouses</option>
                     <option value="Gartland">Gartland Commons</option>
                     <option value="NewTownhouses">New TownHouses</option>
                     </select><br><br>
                     <INPUT TYPE = "Submit" Name = "Submit2" VALUE = "Submit">
                     </Form>');
                }else{
+				// if they are a senior or junior these are the options
                    echo('Here are some options for juniors and seniors:<br>
-                   <Form Name ="toConfirmPage" Method ="POST" ACTION = "index.php">
-                   <select name="Residency">
+                    <Form Name ="toConfirmPage" Method ="POST" ACTION = "index.php">
+                    <select name="Residency">
                     <option value="LowerWest">Lower West</option>
                     <option value="UpperWest">Upper West</option>
                     <option value="FultonStreet">Fulton Street</option>
@@ -65,6 +78,7 @@ if (isset($_POST["Submit"])){
            }                
     else
     {
+		// the if statement that only allows freshman to choose only Leo, Champ, Marian, or Sheahan
         $correct = TRUE;
         if ((($_POST["Residency"]=="Leo")|| 
              ($_POST["Residency"]=="Champ")|| 
@@ -73,6 +87,7 @@ if (isset($_POST["Submit"])){
             echo ("Choose a different location, your housing doesn't match your year.<br>");
             $correct = FALSE;
         }
+		// the if statement that only allows sophomores to choose only Midrise, Foy, Gartland, or New Townhouses
         if ((($_POST["Residency"]=="Midrise")|| 
              ($_POST["Residency"]=="Foy")|| 
              ($_POST["Residency"]=="Gartland")||
@@ -80,6 +95,7 @@ if (isset($_POST["Submit"])){
             echo ("Choose a different location, your housing doesn't match your year.<br>");
             $correct = FALSE;
         }
+		// the if statement that allows juniors and seniors to choose only Lower West, Upper West, Fulton, New Fulton, or Talmadge
         if ((($_POST["Residency"]=="LowerWest")|| 
              ($_POST["Residency"]=="UpperWest")|| 
              ($_POST["Residency"]=="FultonStreet")||
@@ -88,6 +104,7 @@ if (isset($_POST["Submit"])){
             echo ("Choose a different location, your housing doesn't match your year.<br>");
             $correct = FALSE;
         }
+		// the if statement that allows the option of townhouse to be checked and print if this is not what housing was picked
         if ((($_POST["Residency"]=="Leo")|| 
              ($_POST["Residency"]=="Champ")|| 
              ($_POST["Residency"]=="Marian")||
@@ -96,6 +113,7 @@ if (isset($_POST["Submit"])){
             echo ("You want a townhouse, but your selection is not a townhouse.<br>");
             $correct = FALSE;
         }
+		// the if statement that allows the option of kitchen to be checked and print if this is not what housing was picked
         if ((($_POST["Residency"]=="Leo")|| 
              ($_POST["Residency"]=="Champ")|| 
              ($_POST["Residency"]=="Marian")||
@@ -104,16 +122,19 @@ if (isset($_POST["Submit"])){
             echo ("You want a kitchen, but your selection does not have a kitchen.<br>");
             $correct = FALSE;
         }
+		// the button to go forward to the confirmation page if everything is correct
         if ($correct){
             echo (' Your selection fits the criteria, press the button to confirm your selection!
             <Form Name ="toConfirmPage" Method ="POST" ACTION = "index.php">
             <INPUT TYPE = "Submit" Name = "Submit2" VALUE = "Submit">
             </Form>');
         }else{
+		// the button to go back to the previous page
         echo ('<button onclick="window.history.back()">Go back!</button>');
         }
     }
 }
+// the button to submit on the second page after all correct options have been chosen
 else if (isset($_POST["Submit2"])){
     if ($_SESSION["Residency"] == "noSelection")
     {
@@ -126,6 +147,7 @@ else if (isset($_POST["Submit2"])){
     echo ('<button>Confirm</button>');
 }
 else{
+	// the HTML form to select housing options
     echo ('<html>
             <Form Name ="gatherDetails" Method ="POST" ACTION = "index.php">
             Name:<input Type = "Text" name = "fullName"><br><br>
