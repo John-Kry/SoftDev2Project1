de3a5f5ea693af2bb2d5335e6648ac54df87b83c
jibbey <john.kryspin1@marist.edu>
2015-03-04 19:36:32 -0500

    Fully functioning-needs comments

--- index.php
+++ index.php
@@ -8,53 +8,57 @@ if (isset($_POST["Submit"])){
                if (($_POST["Year"]=="Frosh")&&((isset($_POST["Townhouse"]))||(isset($_POST["Kitchen"])))){
                echo ("There is no configuration that exists for these choices.<br>");
                    echo (' Here are some options for freshman:<br> 
+                   <Form Name ="toConfirmPage" Method ="POST" ACTION = "index.php">
                    <select name="Residency">
                    <option value="Leo">Leo Hall</option>
                    <option value="Champ">Champagnat Hall</option>
                    <option value="Marian">Marian Hall</option>
                    <option value="Sheahan">Sheahan Hall</option>
                    </select><br><br>
-                   <Form Name ="toConfirmPage" Method ="POST" ACTION = "index.php">
                    <INPUT TYPE = "Submit" Name = "Submit2" VALUE = "Submit">
                    </Form>');
                }else if ($_POST["Year"]=="Frosh"){
-                   echo (' Here are some options for freshman:<br> <select name="Residency">
+                   echo (' Here are some options for freshman:<br> 
+                   <Form Name ="toConfirmPage" Method ="POST" ACTION = "index.php">
+                   <select name="Residency">
                    <option value="Leo">Leo Hall</option>
                    <option value="Champ">Champagnat Hall</option>
                    <option value="Marian">Marian Hall</option>
                    <option value="Sheahan">Sheahan Hall</option>
                    </select><br><br>
-                   <Form Name ="toConfirmPage" Method ="POST" ACTION = "index.php">
                    <INPUT TYPE = "Submit" Name = "Submit2" VALUE = "Submit">
                    </Form>');
                }else if ($_POST["Year"]=="Soph"&& ((!isset($_POST["Kitchen"]))&&(!isset($_POST["Townhouse"])))){
-                   echo ('Here are some options for sophomores:<br> <select name="Residency">
+                   echo ('Here are some options for sophomores:<br> 
+                   <Form Name ="toConfirmPage" Method ="POST" ACTION = "index.php">
+                   <select name="Residency">
                    <option value="Midrise">Midrise Hall</option>
                     <option value="Foy">Foy Townhouses</option>
                     <option value="Gartland">Gartland Commons</option>
                     <option value="NewTownhouses">New TownHouses</option>
                     </select><br><br>
-                    <Form Name ="toConfirmPage" Method ="POST" ACTION = "index.php">
                     <INPUT TYPE = "Submit" Name = "Submit2" VALUE = "Submit">
                     </Form>');
                }else if (($_POST["Year"]=="Soph") &&((isset($_POST["Kitchen"]))||(isset($_POST["Townhouse"])))){
-                   echo ('Here are some options for sophomores, with a kitchen and or a townhouse:<br> <select name="Residency">
-                    <option value="Foy">Foy Townhouses</option>
+                   echo ('Here are some options for sophomores, with a kitchen and or a townhouse:<br> 
+                    <Form Name ="toConfirmPage" Method ="POST" ACTION = "index.php">
+                    <select name="Residency">
+                   <option value="Foy">Foy Townhouses</option>
                     <option value="Gartland">Gartland Commons</option>
                     <option value="NewTownhouses">New TownHouses</option>
                     </select><br><br>
-                    <Form Name ="toConfirmPage" Method ="POST" ACTION = "index.php">
                     <INPUT TYPE = "Submit" Name = "Submit2" VALUE = "Submit">
                     </Form>');
                }else{
-                   echo('Here are some options for juniors and seniors:<br><select name="Residency">
+                   echo('Here are some options for juniors and seniors:<br>
+                   <Form Name ="toConfirmPage" Method ="POST" ACTION = "index.php">
+                   <select name="Residency">
                     <option value="LowerWest">Lower West</option>
                     <option value="UpperWest">Upper West</option>
                     <option value="FultonStreet">Fulton Street</option>
                     <option value="NewFulton">New Fulton</option>
                     <option value="Talmadge">Talmadge</option>
                     </select><br><br>
-                    <Form Name ="toConfirmPage" Method ="POST" ACTION = "index.php">
                     <INPUT TYPE = "Submit" Name = "Submit2" VALUE = "Submit">
                     </Form>');
                }
@@ -111,6 +115,10 @@ if (isset($_POST["Submit"])){
     }
 }
 else if (isset($_POST["Submit2"])){
+    if ($_SESSION["Residency"] == "noSelection")
+    {
+        $_SESSION["Residency"] = $_POST["Residency"];
+    }
     echo ('Name: '. $_SESSION["fullName"].'<br>');
     echo ('CWID: '. $_SESSION["CWID"]. '<br>');
     echo ('Residential Life Selection: '. $_SESSION["Residency"]. '<br>');
